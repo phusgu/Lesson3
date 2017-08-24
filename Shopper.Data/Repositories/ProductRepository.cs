07a540a473001fce22493c2d9d67aa3f4e3290a2
PHUTON-PC\Administrator <phusgu@gmail.com>
2017-08-24 14:52:55 +0700

    Migration Code first TeduShop

---
+++ Shopper.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Shopper.Data.Infrastructure;
+using Shopper.Model.Models;
+
+namespace Shopper.Data.Repositories
+{
+    public interface IProductRepository
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
